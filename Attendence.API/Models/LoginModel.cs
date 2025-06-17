e0d552d92af492a5ae9be2f54b326a0f6af5b8df
Rachel-Keshet <racheli4265@gmail.com>
2025-06-17 17:23:35 +0300

    הוספת הקבצים של Attendence.API

---
+++ Attendence.API/Models/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AttendenceP.Models
+    {
+        public class LoginModel
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public  string Role { get; set; }
+    }
+    }
+
